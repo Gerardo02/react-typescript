3d86bff8157d4360f86d1b2c15968bcb7cdc01f8
Gerardo02 <54118434+Gerardo02@users.noreply.github.com>
2023-09-13 14:26:18 -0700

    typescript wuhuu

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import TaskList from './components/task_list/TaskList'
+
+function App() {
+
+  return (
+    <>
+      <TaskList />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/task_list/TaskList.tsx
@@ -0,0 +1,49 @@
+import React, { useState } from "react";
+import { Input, Button } from "antd";
+
+interface Task {
+    id: number;
+    description: String;
+    done: boolean;
+
+}
+
+const TaskList: React.FC = () => {
+    const [tasks, setTasks] = useState<Task[]>([]);
+    const [newTask, setNewTask] = useState<string>('');
+
+    const addTask = () => {
+        if(newTask.trim() !== ''){
+            setTasks([...tasks, { id: Date.now(), description: newTask, done: false }]);
+            setNewTask('');
+        }
+    }
+
+    return ( 
+        <>
+            <div>
+                <h1>Lista de tareas</h1>
+
+                <Input 
+                    type="text" 
+                    value={newTask} 
+                    onChange={e => setNewTask(e.target.value)} 
+                    placeholder="Nueva tarea" 
+                />
+
+                <Button onClick={addTask}>Agregar tarea</Button>
+
+                <ul>
+                    {tasks.map((task) => (
+                        <li key={task.id}>
+                            {task.description}
+                        </li>
+                    ))}
+                </ul>
+                
+            </div>
+        </>
+     );
+}
+ 
+export default TaskList;
